eb11dd29a25e07ae4b3a03b18d83411a656af311
fox1s <ostinsmikh@gmail.com>
2021-03-13 13:40:19 +0200

    work users and company (company/serializer have open field 'user' and employee the same)

--- employee/urls.py
+++ employee/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from employee.views import EmployeeListView
+from employee.views import EmployeeListView,EmployeeReadUpdateDeleteView
 
 urlpatterns = [
-    path('', EmployeeListView.as_view(), name='employee_list_create')
+    path('', EmployeeListView.as_view(), name='employee_list_create'),
+    path('/<int:pk>', EmployeeReadUpdateDeleteView.as_view(), name='employee_list_create'),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from employee.models import EmployeeModel
 from employee.serializers import EmployeeSerializer
 
 
 class EmployeeListView(ListCreateAPIView):
+    queryset = EmployeeModel.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeReadUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     queryset = EmployeeModel.objects.all()
     serializer_class = EmployeeSerializer
